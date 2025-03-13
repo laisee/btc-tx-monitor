062ae9b93d3000c2812eff17828d7cbeab91ed84
Gavin Costin <villa.straylite@proton.me>
2025-03-13 10:35:23 +0800

    updated npm++

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// test/test.js
+
+const assert = require('assert');
+describe('BTC dummy test', function() {
+  it('should return true', function() {
+    assert.strictEqual(true, true);
+  });
+});
+
